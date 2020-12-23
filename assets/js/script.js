970cee3a6ba3c8b06814dab236bb7824ecf66ec4
Cesar Martinez <cesaguma@hotmail.com>
2020-12-22 22:40:17 -0500

    Updating functionality for mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,6 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.sidenav');
-//     var instances = M.Sidenav.init(elems, options);
-//   });
-
-  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-  // var collapsibleElem = document.querySelector('.collapsible');
-  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-
-  // Or with jQuery
 
   $(document).ready(function(){
-    $('.sidenav').sidenav();
+    $('.sidenav').sidenav();    
     // Initialize collapse button
     $(".scrollspy").scrollSpy();
     $('.modal').modal();
@@ -19,4 +9,5 @@
       indicators: true
     });
     $('.tooltipped').tooltip();
+    $('.sidenav').sidenav();
   });
